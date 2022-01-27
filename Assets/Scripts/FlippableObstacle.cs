a6973bf2c4fda30b3dea761b8d3993fcbdd982d7
paradoxReborn <96443128+paradoxReborn@users.noreply.github.com>
2022-01-26 18:26:09 -0600

    Implemented collider stretching script
    Obstacles using the FlippableObstacle script and a box collider will now be impassible in 2d view.

---
+++ Assets/Scripts/FlippableObstacle.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlippableObstacle : MonoBehaviour
+{
+    [SerializeField] private DimensionController DC;
+    [SerializeField] private float ExtrudeFactor = 50f;
+
+    private BoxCollider boxCollider;
+
+    private Vector3 originalSize;
+    private bool extruded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        boxCollider = gameObject.GetComponent<BoxCollider>();
+        originalSize = boxCollider.size;
+        if (DC.StartSplit)
+        {
+            ExtrudeForm();
+            extruded = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(extruded != DC.split)
+        {
+            if (extruded)
+            {
+                ResetForm();
+                extruded = false;
+            }
+            else
+            {
+                ExtrudeForm();
+                extruded = true;
+            }
+        }
+    }
+
+    private void ExtrudeForm()
+    {
+        //transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, transform.localScale.z * 50f);
+        boxCollider.size = new Vector3(boxCollider.size.x, boxCollider.size.y, boxCollider.size.z * ExtrudeFactor);
+    }
+
+    private void ResetForm()
+    {
+        //transform.localScale = originalSize;
+        boxCollider.size = originalSize;
+    }
+}
