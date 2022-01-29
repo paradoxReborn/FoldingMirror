5bb73d0f9068dc7a548d5a286fea816292373974
paradoxReborn <96443128+paradoxReborn@users.noreply.github.com>
2022-01-29 13:06:11 -0600

    Return after credits
    Game will return to main menu after credits.

--- Assets/Scripts/CreditScroll.cs
+++ Assets/Scripts/CreditScroll.cs
@@ -2,13 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Roll credits, then return to main menu.
+/// </summary>
 public class CreditScroll : MonoBehaviour
 {
     [SerializeField] private float scrollSpeed = 100;
+    [SerializeField] private float returnToMenuAfter = 60;
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.down * Time.deltaTime * scrollSpeed);
+        transform.Translate(Vector3.up * Time.deltaTime * scrollSpeed);
+    }
+
+    private IEnumerator Start()
+    {
+        yield return new WaitForSeconds(returnToMenuAfter);
+        UnityEngine.SceneManagement.SceneManager.LoadScene(0);
     }
 }
