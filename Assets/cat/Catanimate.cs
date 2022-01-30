9ce46c00b5544f029055cf2d2fb22faed01867b6
paradoxReborn <96443128+paradoxReborn@users.noreply.github.com>
2022-01-29 18:16:08 -0600

    Fixed animation for WASD

--- Assets/cat/Catanimate.cs
+++ Assets/cat/Catanimate.cs
@@ -28,12 +28,12 @@ public class Catanimate : MonoBehaviour
     void Update()
     {
         animator.SetBool("walking",false);
-        if (Input.GetKey("left"))
+        if (Input.GetAxis("Horizontal") < -0.05)
         {
             flipcheck = true;
             animator.SetBool("walking",true);
         }
-        else if (Input.GetKey("right"))
+        else if (Input.GetAxis("Horizontal") > 0.05)
         {
             flipcheck = false;
             animator.SetBool("walking",true);
